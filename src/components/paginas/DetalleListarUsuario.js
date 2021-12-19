b72fe14779afa01f606b78270f8fb85c36f6d12f
JuanCMurcia <vrinda3d@gmail.com>
2021-12-19 12:05:58 -0500

    prueba React

---
+++ src/components/paginas/DetalleListarUsuario.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Sidebar from '../ui/Sidebar';
+
+const DetalleListarUsuario = (   ) => {
+
+
+       return( 
+        <>
+      <div className="md:flex min-h-screen" >
+      <Sidebar />
+      <div className="md:w-2/5 xl:w-4/5 p-6">
+      <h1 className="text-3xl font-light mb-4">Listar Usuarios</h1>
+    
+    </div>
+    </div>
+          
+        
+        </>
+     );
+}
+ 
+export default DetalleListarUsuario;
---
+++ src/components/paginas/Usuarios.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Sidebar from '../ui/Sidebar';
+
+const Usuarios = () => {
+    return (
+        <>
+            <div className="md:flex min-h-screen" >
+                <Sidebar />
+                <div className="md:w-2/5 xl:w-4/5 p-6">
+                    <h1 className="text-3xl font-light mb-4 text-center">Usuarios</h1>
+                    <Link to="/nuevo-usuario" className="  bg-blue-800 hover:bg-blue-700, inline-block mb-5 p-2 text-white uppercase font-bold">
+                        Agregar Usuario
+                    </Link>
+
+                    <Link to="/listar-usuarios" className="  bg-blue-800 hover:bg-blue-700, inline-block mb-5 p-2 text-white uppercase font-bold">
+                        Listar Usuarios
+                    </Link>
+
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Usuarios;
